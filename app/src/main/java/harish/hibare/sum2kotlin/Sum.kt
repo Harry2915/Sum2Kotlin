f51ea4d7eaadffcc89f2044eb4255ead6dcd985e
Harry2915 <harishhibare2915@gmal.com>
2020-11-08 22:38:18 +0530

    solved error

--- app/src/main/java/harish/hibare/sum2kotlin/Sum.kt
+++ app/src/main/java/harish/hibare/sum2kotlin/Sum.kt
@@ -3,18 +3,17 @@ package harish.hibare.sum2kotlin
 import java.util.*
 
 
-object Inputfunctions {
-    @JvmStatic
+
     fun main(args: Array<String>) {
         val readme = Scanner(System.`in`)
-        println("Enter Two Numbers :")
+        println("Enter first Number :")
 
         val n1: Double
         val n2: Double
         val n3: Double
         n1 = readme.nextDouble()
+        println("Enter second Number :")
         n2 = readme.nextDouble()
         n3 = n1 + n2
         println("Total = $n3")
     }
-}
